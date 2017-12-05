f0909c5733ec78842a2bd11e78c08f85010a6fe0
JeK2a <vipjonpc@mail.ru>
2017-12-05 17:12:28 +0300

    Начало разработки функционала

--- Main.py
+++ Main.py
@@ -1,8 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from datetime import *
+
 from cursValut import getCursUSD, getCursEUR
 
 
-class Main:
+if __name__ == "__main__":
     print "USD - ", getCursUSD()
     print "EUR - ", getCursEUR()
 
+    operations = []
+    name = ""
 
+    now = datetime.now()
+    name = input("Введите название расхода/дохода: ")
+    summa = int(input("Ведите сумму расхода/дохода: "))
+    dohodRashod = bool(input("ВВедите 1 если доход / 0 если расход :"))
+
+    oper = (now, name, summa, dohodRashod)
+    print oper
+
+    operations.append(oper)
+    print(operations)
